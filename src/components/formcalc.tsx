29da5e65efab97ee4234f122f19f872c4ce4e694
Sdfeagt <rafalciechanski15@gmail.com>
2023-01-27 16:06:50 +0200

    Further work on the form

--- src/components/formcalc.tsx
+++ src/components/formcalc.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -6,8 +6,8 @@ import Row from 'react-bootstrap/Row';
 import moment from 'moment';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-interface FeeInformation {
+//TODO: Delivery fee shows up without "calculating it". FIXIT
+type FeeInformation = {
   value: number,
   dist: number,
   itemnsNo: number,
@@ -15,32 +15,28 @@ interface FeeInformation {
 }
 
 const FormCalc = () =>{
+  useEffect(()=>{
+  })
   const [validated, setValidated] = useState(false);
-
-  const [deliveryInfo, setDeliveryInfo] = useState({
+  const [deliveryInfo, setDeliveryInfo] = useState<FeeInformation>({
     value: 0.0,
     dist: 0,
     itemnsNo: 0,
     dataTime: ''
   })
 
-
   const handleSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault();
+    event.preventDefault()
     const form = event.currentTarget as HTMLInputElement;
+    console.log(form.checkValidity());
     if (form.checkValidity() === false) {
       event.stopPropagation();
     }
-    setDeliveryInfo({
-      value: 0.0,
-      dist: 0,
-      itemnsNo: 0,
-      dataTime: ''
-    })
     setValidated(true);
   };
 
-  const calcFee = (feeinfo: FeeInformation) =>{
+  const CalcFee = (feeinfo: FeeInformation) =>{
+    if (feeinfo.value != 0 && feeinfo.dist != 0 && feeinfo.itemnsNo != 0){
     let deliveryFee: number = 0
     if (feeinfo.value < 10){
       deliveryFee += 10-feeinfo.value
@@ -57,21 +53,48 @@ const FormCalc = () =>{
     else if (feeinfo.itemnsNo >= 5){
       deliveryFee += (feeinfo.itemnsNo-4) * 0.5
     }
-    //Here implement the friday rush
+    if(getDay(feeinfo.dataTime) && getTime(feeinfo.dataTime)){
+      deliveryFee *= 1.2
+    }
     if(deliveryFee > 15){
       deliveryFee = 15
     }
     if(feeinfo.value >= 100){
       deliveryFee = 0
     }
+    return(<div className='response'>Delivery fee: {deliveryFee}</div>)
+  }
+  else{
+    return(<div className='response'>Nothing</div>)
+  }
   }
 
+  const getDay = (dateString: string) =>{
+    if (new Date(dateString).getDay() === 5){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+  const getTime = (dateString: string) => {
+    let hour: number = Number(dateString.slice(11,13))
+    if (hour >= 15 && hour <= 19){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+
   return (
+    <div className='formcalc'>
     <Form noValidate validated={validated} onSubmit={handleSubmit} className='mb-3'>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="value">
           <Form.Label>Cart value</Form.Label>
-          <Form.Control required type="number" min={0} placeholder="0" defaultValue={0} onChange={(event) => setDeliveryInfo({ ...deliveryInfo, value: Number(event.target.value) })}
+          <Form.Control required type="number" min={0} placeholder="0" onChange={(event) => setDeliveryInfo({ ...deliveryInfo, value: Number(event.target.value) })}
           />
         <Form.Control.Feedback type="invalid">Please provide a valid value.</Form.Control.Feedback>
         <Form.Control.Feedback>Cart value saved!</Form.Control.Feedback>
@@ -80,7 +103,7 @@ const FormCalc = () =>{
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="dist">
           <Form.Label>Delivery distance</Form.Label>
-          <Form.Control required type="number" step={1} min = {0} placeholder="0" defaultValue={0} onChange={(event) => setDeliveryInfo({ ...deliveryInfo, dist: Number(event.target.value) })} />
+          <Form.Control required type="number" step={1} min = {0} placeholder="0" onChange={(event) => setDeliveryInfo({ ...deliveryInfo, dist: Number(event.target.value) })} />
           <Form.Control.Feedback type="invalid">Please provide a valid distance.</Form.Control.Feedback>
           <Form.Control.Feedback>Distance saved!</Form.Control.Feedback>
         </Form.Group>
@@ -88,7 +111,7 @@ const FormCalc = () =>{
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="itemsNo">
           <Form.Label>Number of items</Form.Label>
-          <Form.Control required type="number" step={1} min = {0} placeholder="0" defaultValue={0} onChange={(event) => setDeliveryInfo({ ...deliveryInfo, itemnsNo: Number(event.target.value) })} />
+          <Form.Control required type="number" step={1} min = {0} placeholder="0" onChange={(event) => setDeliveryInfo({ ...deliveryInfo, itemnsNo: Number(event.target.value) })} />
           <Form.Control.Feedback type="invalid">Please provide a valid number of items.</Form.Control.Feedback>
           <Form.Control.Feedback>Number of items saved!</Form.Control.Feedback>
         </Form.Group>
@@ -101,6 +124,8 @@ const FormCalc = () =>{
       </Row>
       <Button type="submit">Calculate delivery price</Button>
     </Form>
+    <CalcFee {...deliveryInfo}/>
+    </div>
   );
 }
 
